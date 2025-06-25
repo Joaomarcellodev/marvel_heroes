78a76d1eae5a62c0c0437a2ed1a31bc838d924a3
Joaomarcellodev <joaomarcellobraz@gmail.com>
2025-06-25 12:43:23 -0300

    pasta repositorio + arquivo hero_repository

---
+++ lib/data/repository/hero_repository.dart
@@ -0,0 +1,18 @@
+import '../models/hero_model.dart';
+import '../services/api_service.dart';
+
+class HeroRepository {
+  final ApiService _apiService;
+
+  HeroRepository(this._apiService);
+
+  Future<List<HeroModel>> fetchHeroes() => _apiService.fetchHeroes();
+
+  Future<HeroModel> addHero(HeroModel hero) => _apiService.addHero(hero);
+
+  Future<HeroModel> updateHero(HeroModel hero) => _apiService.updateHero(hero);
+
+  Future<void> deleteHero(int id) => _apiService.deleteHero(id);
+
+  List<HeroModel> getLocalHeroes() => _apiService.getLocalHeroes();
+}
